55dee5c5c0f266269660be93cf57483437f9bfe3
Marco Chiappetta <chpmrc@gmail.com>
2017-07-04 11:56:35 +0300

    Support eth

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ const bot = new TelegramBot(token, {polling: true});
 
 try {
   db = JSON.parse(db);
+  db.lastRates = db.lastRates || {};
 } catch (e) {
-  db = {subscribers: {}, last: null};
+  db = {subscribers: {}, last: null, lastRates: {}};
 }
 
 const sleep = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
@@ -39,16 +40,17 @@ function unsubscribe(chatId) {
   commit();
 }
 
-function updateLast(last) {
-  db.last = last;
+function updateLast(last, what) {
+  db.lastRates[what] = last;
   commit();
 }
 
 bot.onText(/\/start/, (msg, match) => {
   bot.sendMessage(msg.chat.id, 'This is an EXPERIMENTAL personal project.');
-  bot.sendMessage(msg.chat.id, 'Send /below THRESHOLD to be warned when the rate goes below the threshold');
-  bot.sendMessage(msg.chat.id, 'Send /above THRESHOLD to be warned when the rate goes above the threshold');
-  bot.sendMessage(msg.chat.id, 'Send yo to get the last rate');
+  bot.sendMessage(msg.chat.id, 'Send /below THRESHOLD to be warned when the BTC-EUR rate goes below the threshold');
+  bot.sendMessage(msg.chat.id, 'Send /above THRESHOLD to be warned when the BTC-EUR rate goes above the threshold');
+  bot.sendMessage(msg.chat.id, 'Send btc to get the last BTC-EUR rate');
+  bot.sendMessage(msg.chat.id, 'Send btc to get the last ETH-EUR rate');
   bot.sendMessage(msg.chat.id, 'Send /stop to unsubscribe');
 });
 
@@ -72,28 +74,38 @@ bot.onText(/\/stop/, (msg, match) => {
   bot.sendMessage(chatId, 'Unsubscribed, bye');
 });
 
-bot.onText(/yo/i, (msg, match) => {
+bot.onText(/btc/i, (msg, match) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, `Last rate: ${db.last}`);
+  bot.sendMessage(chatId, `Last ETH-EUR rate: ${db.lastRates.btc}`);
 });
+
+bot.onText(/eth/i, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, `Last ETH-EUR rate: ${db.lastRates.eth}`);
+});
+
 
 
 async function main() {
   let res = null;
+  let last = null;
   while (true) {
 
     try{
       res = await axios.get(apiBase + '/products/BTC-EUR/stats');
-      console.log(res.data);
-      const last = res.data.last;
-      updateLast(last);
+      last = res.data.last;
+      updateLast(last, 'btc');
+      res = await axios.get(apiBase + '/products/ETH-EUR/stats');
+      last = res.data.last;
+      updateLast(last, 'eth');
+      console.log('Last rates: ', db.lastRates);
       for (let subscriber in db.subscribers) {
         const sub = db.subscribers[subscriber];
-        if (last > sub.above && sub.above !== null) {
-          bot.sendMessage(subscriber, `Last BTC-EUR rate was ${last} (above ${sub.above})`);
+        if (db.lastRates.btc > sub.above && sub.above !== null) {
+          bot.sendMessage(subscriber, `Last BTC-EUR rate was ${db.lastRates.btc} (above ${sub.above})`);
         }
-        if (last < sub.below && sub.below !== null) {
-          bot.sendMessage(subscriber, `Last BTC-EUR rate was ${last} (below ${sub.below})`);
+        if (db.lastRates.btc < sub.below && sub.below !== null) {
+          bot.sendMessage(subscriber, `Last ETH-EUR rate was ${db.lastRates.btc} (below ${sub.below})`);
         }
       };  
     } catch (e) {
